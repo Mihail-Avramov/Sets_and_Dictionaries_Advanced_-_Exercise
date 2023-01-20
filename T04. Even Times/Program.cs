47a750e87de0e525ee0edd7214d55d3b6cdcbcfa
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-01-20 10:46:16 +0200

    T04

---
+++ T04. Even Times/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+int numberCount = int.Parse(Console.ReadLine());
+
+Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+for (int i = 0; i < numberCount; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+
+    if (!numbers.ContainsKey(number))
+    {
+        numbers.Add(number, 0);
+    }
+
+    numbers[number]++;
+}
+
+
+//Console.WriteLine(numbers.First(x => x.Value % 2 == 0).Key);
+Console.WriteLine(numbers.Single(x => x.Value % 2 == 0).Key);
