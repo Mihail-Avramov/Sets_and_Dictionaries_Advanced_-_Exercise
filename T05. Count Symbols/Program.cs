d6ae3e02a8e2095123f215d318f5283179e3128a
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-01-20 15:09:35 +0200

    T05

---
+++ T05. Count Symbols/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+string text = Console.ReadLine();
+
+SortedDictionary<char,int> charCounter = new SortedDictionary<char, int>();
+
+foreach (var ch in text)
+{
+    if (!charCounter.ContainsKey(ch))
+    {
+        charCounter[ch] = 0;
+    }
+
+    charCounter[ch]++;
+}
+
+foreach (var (ch, count) in charCounter)
+{
+    Console.WriteLine($"{ch}: {count} time/s");
+}
