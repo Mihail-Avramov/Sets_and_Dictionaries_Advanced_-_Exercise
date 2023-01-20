0d5ff007a7edeecef9c29e15b7032159060869aa
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-01-20 08:08:54 +0200

    T02

---
+++ T02. Sets of Elements/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+HashSet<int> firstSet = new HashSet<int>();
+HashSet<int> secondSet = new HashSet<int>();
+
+string[] input = Console.ReadLine().Split(' ');
+
+int firstNumber = int.Parse(input[0]);
+int secondNumber = int.Parse(input[1]);
+
+for (int n = 0; n < firstNumber; n++)
+{
+    firstSet.Add(int.Parse(Console.ReadLine()));
+}
+
+for (int m = 0; m < secondNumber; m++)
+{
+    secondSet.Add(int.Parse(Console.ReadLine()));
+}
+
+firstSet.IntersectWith(secondSet);
+
+Console.WriteLine(string.Join(" ", firstSet));
