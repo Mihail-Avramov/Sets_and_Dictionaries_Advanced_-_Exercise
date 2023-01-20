981df4e7dd224354203966f0fa4ae06c9d3c40b0
Mihail Avramov <mihail.a.avramov@gmail.com>
2023-01-20 08:29:38 +0200

    T03

---
+++ T03. Periodic Table/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+int counter = int.Parse(Console.ReadLine());
+
+SortedSet<string> periodicTable = new SortedSet<string>();
+
+for (int i = 0; i < counter; i++)
+{
+    string[] chElements = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    foreach (var element in chElements)
+    {
+        periodicTable.Add(element);
+    }
+}
+
+Console.WriteLine(string.Join(" ", periodicTable));
